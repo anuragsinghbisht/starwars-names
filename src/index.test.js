17bcd47041e4708d268ac0f21e8bfa890942195c
Anurag Singh Bisht <anurag.bisht@imaginea.com>
2017-06-22 14:09:47 +0530

    chore(releasing): Add travis config, conventional commit, semantic-release
    BREAKING CHANGE: closes #1
    
    closes #1

---
+++ src/index.test.js
@@ -0,0 +1,28 @@
+const expect = require("chai").expect;
+const starwars = require("./index");
+
+describe("starwars-names-generator", () => {
+  describe("all", () => {
+    it("should be an array of strings", () => {
+      const isArrayOfStrings = array => {
+        return array.every(function(item) {
+          return typeof item === "string";
+        });
+      };
+
+      expect(starwars.all).to.satisfy(isArrayOfStrings);
+    });
+
+    it('should contain "Luke Skywalker"', () => {
+      expect(starwars.all).to.include("Luke Skywalker");
+    });
+
+  });
+
+  describe('random', () => {
+    it('should return a random item from the starwars.all', () => {
+      const randomItem = starwars.random();
+      expect(starwars.all).to.include(randomItem);
+    });
+  });
+});
